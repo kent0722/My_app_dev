0ae8402a6529670a5c91c92b61bef74fccb108b5
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-08-02 02:35:55 +0900

    test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :require_login
   
   def index
-    @posts = Post.all.includes(:user).order(created_at: :desc)
+
   end
 
   def show; end
@@ -12,13 +12,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.posts.build(post_params)
-     Rails.logger.debug "Post params: #{post_params.inspect}"
-    if @post.save
-      redirect_to posts_path, flash: { notice: '投稿しました' }
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
 
   def edit; end
@@ -30,6 +23,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:brand, :category, :production_year, :instrument_model, :image, :video, :body,)
+
   end
 end
